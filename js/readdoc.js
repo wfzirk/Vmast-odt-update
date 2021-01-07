a8e973c532b899c5bca9034676cd9d22c3147914
wfzirk <42407520+wfzirk@users.noreply.github.com>
2021-01-07 06:25:35 -0600

    Initial commit

---
+++ js/readdoc.js
@@ -0,0 +1,291 @@
+
+
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+  return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+}
+
+/*
+function chunkit(str) {
+	var chunks = [];
+
+	for (var i = 0, charsLength = str.length; i < charsLength; i += 2) {
+		chunks.push(str.substring(i, i + 2));
+	}
+	return chunks
+}
+*/
+
+function UTF8ToText(x){
+	var result = "";
+	//console.log(x)
+	try {
+		//if(chkutf8){
+		//    x = x.toString();
+		//    for (var i = 0; (i < x.length && x.substr(i, 2) !== '00'); i += 2)
+		//        result += String.fromCharCode(parseInt(x.substr(i, 2), 16));
+		//} else {
+			x = x.replace(/\\x/gi,"");
+			x = x.toString();
+			for (var i = 0; (i < x.length && x.substr(i, 2) !== '00'); i += 2)
+				result += String.fromCharCode(parseInt(x.substr(i, 2), 16));
+	   // }
+		result = decodeURIComponent(escape(result));
+	} catch(err) {
+		return err.message;
+	} finally {
+		return result;
+	}
+}
+
+
+const whitespaceCharacters = [' ', '  ',
+  '\b', '\t', '\n', '\v', '\f', '\r', `\"`, `\'`, `\\`,
+  '\u0008', '\u0009', '\u000A', '\u000B', '\u000C',
+'\u000D', '\u0020','\u0022', '\u0027', '\u005C',
+'\u00A0', '\u2028', '\u2029', '\uFEFF']
+
+// Use includes method on string
+function hasWhiteSpace(s) {
+  const whitespaceChars = whitespaceCharacters;
+  return whitespaceChars.some(char => s.includes(char));
+}
+	
+	
+function optionBox(searchWords, selText) {	
+	var modal = document.getElementById("myModal");
+	var span = document.getElementsByClassName("close")[0];
+	var para = document.getElementById("mselWord")
+	para.innerHTML = "Select SUN word to replace <b>"+selText+"</b>";
+	modal.style.display = "block";
+	console.log("optionbox")
+	console.log('searchwords',searchWords.length);
+	var select = document.getElementById("selectElementId");
+	select.innerHTML = "";
+	select.size = 0;
+	//select.onchange = re
+	for (var i in searchWords) {
+		var [newWord, unicode] =searchWords[i].split(' ');
+		unicode = unicode.replace('(','').replace(')','');
+		select[i] = new Option(newWord,unicode,false,false);
+	}
+	select[0].defaultSelected = true;
+	select.size=select.options.length;
+	inp = document.getElementById("selBtn");
+	inp.onclick = replaceText;
+	cncl = document.getElementById("cnclBtn");
+	cncl.onclick = function() {modal.style.display="none";}
+
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+}
+	
+	
+function replaceText(sel) {
+		var modal = document.getElementById("myModal");
+		console.log('replaceText')
+		var sel = document.getElementById("selectElementId");
+		var text= sel.options[sel.selectedIndex].text;
+		var val = sel.options[sel.selectedIndex].value;
+		console.log(text, val);
+
+		console.log('replaceText',unicode, text);
+		var uChar = unescape('%u'+val)
+		var winSel = window.getSelection();
+		if (winSel) {
+		   if (winSel.rangeCount) {
+				range = winSel.getRangeAt(0); 
+				console.log(range)
+				range.deleteContents();
+				range.insertNode(document.createTextNode(uChar));
+			}
+		} else if (document.selection && document.selection.createRange) {
+			range = document.selection.createRange();
+			range.text = replacementText;
+		}
+		modal.style.display = "none";
+
+		// remove onclick event	
+		inp = document.getElementById("selBtn").onclick="";
+		sel.innerHTML = "";
+	}		
+	
+	
+	
+function getSelectedText(replacementText) {
+	// https://stackoverflow.com/questions/3997659/replace-selected-text-in-contenteditable-div
+
+	if (!table_loaded) { 
+		alert("Dictionary file has not been loaded.");
+		return;
+	}	
+	var  range;
+	var winSel = window.getSelection();
+	var selText = winSel.toString();
+	var selHex = toHex(selText).toLowerCase();
+
+	if (hasWhiteSpace(selText)) {
+		//alert('Invalid selection.  Choose Ascii word');
+		return;
+	}
+	
+	if (selHex.length === 0) {
+		//alert('Invalid selection.  Choose Ascii word');
+		return;
+	}
+	
+	if (selHex.length === 4 && selHex[0] ==='e') {
+		//alert('Invalid selection.  Choose Ascii word');
+		return;
+	}	
+	
+	var searchWords = []
+	searchWords = search_Table(selText);
+	
+	//================================
+	if (searchWords.length > 0) optionBox(searchWords, selText);
+
+}
+
+
+		   
+function getAllMatchesArray(needle, haystack) {
+	console.log('allmatches',needle, typeof haystack)
+	var index = [];   //, i = -1; 
+	
+	for (var i in haystack) {
+		console.log(haystack[i].length, haystack[i][1])
+		if (haystack[i].length < 3) continue
+		console.log('haystack',i,haystack[i][1], needle, haystack[i][2], haystack[i][1].includes(needle))
+		if (haystack[i][1].includes(needle)) {
+			console.log('match at',i)
+			index.push([i,needle, haystack[i][1], haystack[i][2]])
+		}
+	}
+	console.log('getallmatchesarray index ', index)
+	return index; 
+} 
+
+function getAllMatchesStr(str, searchToken, preIndex, output){
+	var result = str.match(searchToken);
+	console.log('getallmatchesstr',searchToken,'preindex',preIndex)
+	if(result){
+		output.push(result.index +preIndex);
+		str=str.substring(result.index+searchToken.length);
+		getAllMatchesStr(str, searchToken, preIndex, output)
+	}
+	console.log('getallmatchesstr',result, output)
+    return output;
+};
+
+function matchAll(needle, haystack) {
+	return [...haystack.matchAll(new RegExp(needle, 'gi'))].map(a => a.index);
+}
+
+function onmouseoverspan(){
+		this.style.backgroundColor = "red";
+	}
+function onmouseoutspan(){
+		this.style.backgroundColor = "transparent";
+	}
+
+	
+function readDoc(docStr, csvArray) {
+	sepstr = "#"
+	console.log('readdoc',docStr.length, csvArray.length);
+	var allIndexArray = []
+	var wordList = {}
+	//try{
+	docStr = docStr.replace(/\s/g, '#')
+	docStr = docStr.replace(/'s/g, '#@')
+		//console.log(docStr)
+		//console.log(csvArray)
+		for (var i in csvArray) {
+			//if (i === 0) continue
+			if (csvArray[i].length < 3) continue
+			//console.log(csvArray[i].length, csvArray[i])
+			line = csvArray[i]   //.split(',')
+			word = line[1]
+			unicode = line[2].toLowerCase();
+			//console.log(i, word, unicode)
+			//if (i === 0) continue;
+			if (unicode === 'e390') continue;
+			if (unicode === 'e316') continue;
+			if (unicode === 'e37e') continue;
+			
+			searchword  = '#'+word+'#'
+			
+
+			//if (i < 10) {
+			//	console.log('searchword',searchword, unicode);
+			//}
+			if (searchword.includes('_')) {
+				c = searchword.split('_')
+				//console.log('split',c[0], c[1])
+				searchword = c[0]+'#'
+				//console.log('contains _',searchword);
+			}
+
+			var n = docStr.indexOf(searchword);
+			//console.log(docStr)
+			if (n > -1) {
+				indexes = []
+				indexes.push(unicode)
+				//indexes.push([...docStr.matchAll(new RegExp(searchword, 'gi'))].map(a => a.index);
+				 // [2, 25, 27, 33]
+				indexes.push(matchAll(searchword, docStr))
+
+				wordList[searchword] = indexes
+				console.log('wordmatchesstr',searchword, wordList)
+				
+			}
+		}
+
+		console.log(wordList)
+
+	markAllWords(wordList, allIndexArray)
+}	
+		
+String.prototype.replaceAll = function (find, replace) {
+    var str = this;
+	console.log('replaceall', find, replace);
+    return str.replace(new RegExp(find, 'g'), replace);
+};
+
+function markAllWords(wordList, arrayList) {
+	console.log('markallwords', wordList, arrayList)	
+	//try {
+		var inner = document.getElementById("output").innerHTML;
+		for (var word in wordList) {
+			console.log(word, wordList[word])
+			wrd = wordList[word]
+			uni = wrd[0]
+			var unicode = unescape('%u' + uni);
+			offsets = wrd[1]
+			//for (var i in offsets){
+				word = word.replace(/#/g,'')
+				console.log("word",word, "unicode", uni, "offsets",offsets)
+				p = inner.indexOf(word)
+				//console.log(p,word,toHex(inner))
+				//inner = inner.replaceAll(word, '<mark>'+unicode+'</mark>');
+				inner = inner.replaceAll(word, '<mark>'+word+'</mark>');
+			//}
+		}	
+		//console.log(inner)
+		document.getElementById("output").innerHTML = inner;
+		//console.log(inner)
+		
+	//} catch(err) {
+	//	console.log('error',err.message);
+		//console.log('   ',i, csvArray[i], csvArray[i].length,searchword, unicode);
+	//}
+		
+
+}	
---
+++ js/table.js
@@ -0,0 +1,271 @@
+var fontCol = 0;
+var uniCol = 2;
+var nameCol = 1;
+//var synCol = 4;
+//var xrefCol = 3;
+refCol = 3;
+var noCols = 3;
+	
+// find font column and unicode column	
+function findCol(arry) {
+	console.log('findcol',arry.length);
+	var t0 = performance.now();
+	var found = false;
+	var rowx = 0;
+	for (var i = 4; i < arry.length; i ++) {
+		rowx = arry[i]
+		console.log(i, rowx.length, rowx);
+		for (var j = 0;  j < rowx.length; j++) {  //  eeac8d = eb0d
+			//	https://stackoverflow.com/questions/17267329/converting-unicode-character-to-string-format
+			var	uni = rowx[j].charCodeAt(0).toString(16).toUpperCase();
+			//console.log(i, j, '|'+uni+'|', uni.length);
+		    if (uni.length == 4) { 
+				fontCol = j;
+				for (var k = 0;  k < rowx.length; k++){
+					if (k === fontCol) continue;
+					//console.log(j,k,uni, rowx[k], rowx[k].length, fontCol);
+					if (rowx[k].toUpperCase() === uni) {
+						console.log('found')
+						uniCol = k;
+						found = true;
+						break;
+					}
+				}
+			}	
+		}
+		if (i > 16) break;   
+		if (found) break;
+	}
+	noCols = arry[5].length;
+	if (fontCol === 0 && uniCol == 3) {
+		nameCol = 2
+		refcol = 4
+
+	} 	
+		
+	console.log("noCols %d font %d name %d uni %d, ref %d",noCols,fontCol, nameCol, uniCol, refCol);
+	var t1 = performance.now();
+	console.log("findCol " + (t1 - t0) + " milliseconds.");
+}
+
+function generateTable(lines){
+	//Clear previous data
+	findCol(lines)
+	console.log('generateTable');
+	var t0 = performance.now();
+	document.getElementById("dict").innerHTML = "";
+	var table = document.createElement("table");
+	table.id = "searchtable";
+	table.className = 'xreftable';
+	var len = 0;
+	for (var i = 0; i < lines.length; i++) {
+	  // find longest row
+		if (lines[i].length > len) {
+			len = lines[i].length;
+		}
+	}
+	
+	table.createCaption();
+	table.caption.innerHTML = lines[0];
+	// make header
+	var row = document.createElement('TR');
+	for (var j = 1; j < len; j++) {
+		var th = document.createElement("TH");
+		th.appendChild(document.createTextNode('col '+j));
+		var fontCol = 0;
+		/*
+		if (j === fontCol) {
+			th.className = "fonticon";
+			th.innerHTML = "Font";
+		}*/
+		if (j != nameCol) continue;
+		if (j === uniCol) {
+			th.className = "unicol";
+			th.innerHTML = "Unicode";
+		}
+		if (j === nameCol) {
+			th.className = "nameCol";
+			th.innerHTML = "Name";
+		}
+		/*
+		if (j === refCol) {
+			th.className = "refCol";
+			th.innerHTML = "Reference";
+		}
+		*/
+		row.appendChild(th);
+	}
+
+	
+	table.appendChild(row);
+	var tbody = document.createElement('TBODY');
+	for (var i = 1; i < lines.length; i++) {  // get line
+		if (lines[i].length > 1) {	// process row
+			var row = document.createElement('TR');
+			row.className = "item";
+			//var mismatch = false;
+			var td = document.createElement("TD");
+			text = lines[i][nameCol]+' ('+lines[i][uniCol].toUpperCase()+')';
+			td.appendChild(document.createTextNode(text));
+			row.appendChild(td)
+			row.style.display = "";
+			
+			
+		}	// end row process
+		tbody.appendChild(row);
+	}  // end process line
+	table.appendChild(tbody);
+	document.getElementById("dict").appendChild(table);
+	console.log(table)
+	var t1 = performance.now();
+	console.log("generateTable " + (t1 - t0) + " milliseconds.");
+	
+	/*
+	table.addEventListener('click', function(event) {
+		const row = event.target.parentNode; 
+		console.log(row.textContent)
+	}); */
+	return table;
+}
+
+
+function toHex(str) {
+	var result = '';
+	for (var i=0; i<str.length; i++) {
+		result += str.charCodeAt(i).toString(16);
+	}
+	//console.log('toHex', str, result);
+	return result;
+}
+
+function toHexArray(str) {
+	var result = [];
+	for (var i=0; i<str.length; i++) {
+		result.push(str.charCodeAt(i).toString(16));
+	}
+	//console.log('toHexArr', str, result);
+	return result;
+}
+
+
+function jscsvToArray(text) {
+	console.log('xcsv...')
+	row = [];
+	lines = text.split('\n');
+	for (var i in lines){
+		//l = lines[i].replace(/|/g, ",");
+		l = lines[i].split('|')
+		for (var j in l) {
+			l[j] = l[j].substring(1, l[j].length - 1)
+		}
+		row.push(l);
+	}
+	console.log(row[2])
+	return row;
+}
+
+function csvToArray(text) {
+	var t0 = performance.now();
+    let p = '', row = [''], ret = [row], i = 0, r = 0, s = !0, line;
+   for (line of text) {
+	    if ('"' === line) {
+            if (s && line === p) {
+				row[i] += line;
+			}
+            s = !s;
+        } else if (',' === line && s) {
+      		line = row[++i] = '';
+		} else if ('\n' === line && s) {
+            if ('\r' === p) {
+				row[i] = row[i].slice(0, -1);
+			}
+            row = ret[++r] = [line = '']; i = 0;
+        } else {
+			row[i] += line;
+		}
+        p = line;
+    }
+	//console.log('csv2array',ret[4]);
+	var t1 = performance.now();
+	console.log("csvToArray " + (t1 - t0) + " milliseconds.");
+    return ret;
+};
+
+function search_Table(input){
+	search_words = []
+	//srchType = 'char';
+	input = input.toUpperCase()
+	console.log('searchTable', input);
+	//var input = document.getElementById('xsearch').value.toUpperCase();
+	var filter =  input.split(' '); 
+	console.log('searchtable',srchType, input, filter)	
+	table = document.getElementById("searchtable");
+	tr = table.getElementsByTagName("tr");
+	for (i = 1; i < tr.length; i++) {
+		td = tr[i].getElementsByTagName("td") ; 
+		var txt = "+";
+		for(j=0 ; j < td.length ; j++) {
+			  let tdata = td[j] ;
+			  if (tdata) {
+				  //console.log(j, tdata.innerHTML)
+				  t = tdata.innerHTML.split(' ')[0];  // strip off UNICODE not compare on unicode
+				 //txt = txt +'+'+ tdata.innerHTML.toUpperCase
+				 txt = txt +'+'+ t.toUpperCase();
+			  }
+			  //console.log('td.length',td.length, txt)
+		}
+		//console.log('srch',txt, 'filter',filter);
+		if (srchType === 'word') {  // word search
+			txt = txt +'+';
+			txt = txt.replace(/ /g,'+')
+			txt = txt.replace(/:/g,'+')
+			txt = txt.replace(/,/g,'+')
+			//txt = txt.split('+')
+			//txt = txt.replace('++','')
+			var found = true;
+			for(var f = 0; f < filter.length; f++) {
+				//console.log('filter[f]', filter[f],'txt', txt,'input', input)
+				if (txt.indexOf('+'+filter[f]+'+')  === -1) { 
+					found = false;
+				}
+				
+				//if (txt.startsWith('+'+filter[f]+'+')  === -1) { 
+				//	found = false;
+				//}
+			}
+		} else {			// char search
+			var found = true;
+			for(var f = 0; f < filter.length; f++) {
+				s = '++'+filter[f]
+				
+				//console.log('filter[f]', s,'txt', txt,'input', input, txt.startsWith(s))
+				if (txt.indexOf(filter[f])  === -1) { 
+					found = false;
+				}
+				if (!txt.startsWith(s) ) { 
+					found = false;
+				}
+			}
+		}	
+		//found = arrayContains(txt, filter)
+		//console.log(i,input, txt, found)
+		if (found) {
+				tr[i].style.display = "";	
+				search_words.push(tr[i].textContent);
+		} else {
+				tr[i].style.display = "none";
+		}
+		
+	}
+	//console.log('search_words',search_words);
+	table.style.display="none";
+	return search_words;
+}
+
+function clearTable() {
+    var input, filter, found, srchtable, tr, td, i, j;
+    document.getElementById('xsearch').value = "";
+	search_Table();
+//	document.getElementById('xlf').value = ""; 
+	console.log("clearall");
+}
